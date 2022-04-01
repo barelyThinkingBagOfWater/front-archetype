0946fc8420247ee5a7a20c99e73cc456f5a8bdb4
One barely thinking bag of water, on a big rock somewhere in the universe <69302809+barelyThinkingBagOfWater@users.noreply.github.com>
2022-04-01 12:20:16 +0800

    Adapting code to React18

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render } from "react-dom";
+import { createRoot } from 'react-dom/client';
 import { Provider } from "react-redux";
 
 import store from "./js/store/index";
@@ -7,9 +7,11 @@ import App from "./js/components/App.jsx";
 
 import "./css/App.css";
 
-render(
+const container = document.getElementById("root")
+const root = createRoot(container);
+
+root.render(
     <Provider store={store}>
         <App/>
-    </Provider>,
-    document.getElementById("root")
+    </Provider>
 );
